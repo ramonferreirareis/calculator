0d753342ecc30be9adcb0714e46ec468f13360ae
Ramon Reis <111209853+ramonferreirareis@users.noreply.github.com>
2024-12-03 21:10:45 -0300

    Add function to call one of the operations based on the operator variable

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let firstNumber = 2;
+let secondNumber = 3;
+let operator = "+";
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -12,4 +16,11 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+function operate() {
+    if (operator === "+") add(firstNumber, secondNumber);
+    if (operator === "-") subtract(firstNumber, secondNumber);
+    if (operator === "x") multiply(firstNumber, secondNumber);
+    if (operator === "÷") divide(firstNumber, secondNumber);
 }
